f49e227c6db6958e1e5e7742e8cf8f7c4178e0b4
Uhyun <96884119+uhyunpark@users.noreply.github.com>
2024-02-02 16:24:03 +0900

    proto lint

--- x/posa/client/cli/utils.go
+++ x/posa/client/cli/utils.go
@@ -9,17 +9,17 @@ import (
 	"github.com/settlus/chain/x/posa/types"
 )
 
-// ParseCreateValidatorData reads and parses a ParseCreateValidatorData from a file.
-func ParseCreateValidatorData(metadataFile string) (*types.ValidatorInfo, error) {
+// ParseCreateValidatorData reads and parses a ParseCreateValidatorData from a given file.
+func ParseCreateValidatorData(fileName string) (*types.ValidatorInfo, error) {
 	validatorInfo := types.ValidatorInfo{}
 
-	contents, err := os.ReadFile(filepath.Clean(metadataFile))
+	data, err := os.ReadFile(filepath.Clean(fileName))
 	if err != nil {
 		return nil, err
 	}
 
-	if err = json.Unmarshal(contents, &validatorInfo); err != nil {
-		return nil, fmt.Errorf("failed to unmarshal proposal metadata: %w", err)
+	if err = json.Unmarshal(data, &validatorInfo); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal validator info: %w", err)
 	}
 
 	return &validatorInfo, nil
